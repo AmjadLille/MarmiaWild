83e340a286f88aded500a073e3ffc118a9cf31f5
AmjadWebDev <amjadwebdev@gmail.com>
2020-10-05 14:16:18 +0200

    add up button

---
+++ button-move-up/button-move-up.js
@@ -0,0 +1,25 @@
+(function () {
+  const trackScroll = () => {
+    let scrolled = window.pageYOffset;
+    let coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      goTopBtn.classList.add("back_to_top-show");
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove("back_to_top-show");
+    }
+  };
+
+  const backToTop = () => {
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -80);
+      setTimeout(backToTop, 0);
+    }
+  };
+
+  let goTopBtn = document.querySelector(".back_to_top");
+
+  window.addEventListener("scroll", trackScroll);
+  goTopBtn.addEventListener("click", backToTop);
+})();
